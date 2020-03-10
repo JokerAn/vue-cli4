7be4437cbe75a3c1d9d4e0d2363b3e55f714bf8d
JokerAn <divfooter@163.com>
2020-03-10 16:16:48 +0800

    除了基本的token 有些接口需要额外的标识

--- src/apis/login/index.js
+++ src/apis/login/index.js
@@ -4,6 +4,10 @@ export function loginApi(data){
   return myAxios({
     url: '/cms/admin/login',
     method: 'post',
-    data
+    data,
+    // 除了基本的token 有些接口需要额外的标识
+    headers: {
+      'App-version': 1
+    }
   })
 }
